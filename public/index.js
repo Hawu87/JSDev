b30d8196f32c9c5dea4174e6a5e75f08d74462fb
Hawulethu Ndlovu <hawundlovu87@gmail.com>
2025-11-17 15:30:54 -0600

    Added the window alert

--- public/index.js
+++ public/index.js
@@ -1,3 +1,3 @@
 // console.log(`Hie this is running`);
 
-// window.alert('This is an alert'); //This is the alert that pulls up on top of the web page
+window.alert('This is an alert'); //This is the alert that pulls up on top of the web page
